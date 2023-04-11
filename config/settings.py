4bd8e58cda63d2265dbc5e04122662e4bd47b537
abd-Kabir <kabirumarov@gmail.com>
2023-04-11 14:32:55 +0500

    commit

--- config/settings.py
+++ config/settings.py
@@ -75,19 +75,19 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
     # 'default': {
-    #     'ENGINE': 'django.db.backends.postgresql',
-    #     'NAME': 'track',
-    #     'USER': getenv("POSTGRES_USER"),
-    #     'PASSWORD': getenv("POSTGRES_PASSWORD"),
-    #     # 'PASSWORD': 'o030101',
-    #     'HOST': 'localhost',
-    #     'PORT': '',
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
     # }
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'track',
+        'USER': getenv("POSTGRES_USER"),
+        'PASSWORD': getenv("POSTGRES_PASSWORD"),
+        # 'PASSWORD': 'o030101',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
 }
 
 # Password validation
