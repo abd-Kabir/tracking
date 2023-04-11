85423c7372027187c4b25c16f28029cde4a43b39
abd-Kabir <kabirumarov@gmail.com>
2023-04-12 04:51:33 +0500

    commit

--- config/settings.py
+++ config/settings.py
@@ -16,7 +16,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'localhost:8000', 'localhost', 'uzll.uz:8000/', 'uzll.uz', '80.87.202.231', '80.87.202.231:8000',
